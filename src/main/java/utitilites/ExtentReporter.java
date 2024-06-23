60701cdf383607261e0cea1ab89a8db1550d2b1a
Santosh1295 <chougulesantosh95@gmail.com>
2024-06-23 17:26:32 +0530

    utilities commit

--- src/main/java/utitilites/ExtentReporter.java
+++ src/main/java/utitilites/ExtentReporter.java
@@ -9,13 +9,13 @@ public class ExtentReporter {
 		
 		String extentReportPath = System.getProperty("user.dir")+"\\reports\\extentreport.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(extentReportPath);
-		reporter.config().setReportName("TutorialsNinja Automation Results");
-		reporter.config().setDocumentTitle("TutorialsNinja Automation Test Results");
+		reporter.config().setReportName("Amazon Automation Results");
+		reporter.config().setDocumentTitle("Amazon Automation Test Results");
 		
 		ExtentReports extentReport = new ExtentReports();
 		extentReport.attachReporter(reporter);
 		extentReport.setSystemInfo("Operating System","Windows 11");
-		extentReport.setSystemInfo("Executed By","Arun Motoori");
+		extentReport.setSystemInfo("Executed By","Santosh Chougule");
 		
 		return extentReport;
 	}
